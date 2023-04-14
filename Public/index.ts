56e1fada672ab4c1e7267392d599e3b31c219928
ZaChiDev <ZaChiDev@gmail.com>
2023-04-14 21:12:24 +0200

    1

--- Public/index.ts
+++ Public/index.ts
@@ -1,5 +1,13 @@
+function Nav( id="NavContext", expand=false ) {
+	let eNav;
+	if ( ( eNav = document.getElementById( id ) ) )
+		eNav.className = expand ? "expand" : "";
+}
+
 (function(){
 	let eCurrentYear;
 	if ( ( eCurrentYear = document.getElementById( "CurrentYear" ) ) )
 		eCurrentYear.innerHTML = new Date().getFullYear().toString();
+
+	Nav();
 })();
