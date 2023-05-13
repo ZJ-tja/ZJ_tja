32d3d4dc11546b0aaf87fe73e7140351d505f539
ZaChiDev <ZaChiDev@gmail.com>
2023-05-13 15:32:11 +0200

    GoingDeeper

--- Public/index.ts
+++ Public/index.ts
@@ -15,6 +15,18 @@ function Nav( open = false, type ="context" ) {
 	}
 }
 
+function ScrollTo( id: string ) {
+	let ctx, target;
+	if ( !( ctx = document.getElementById( "Context" ) ) )
+		return;
+	
+	if ( !( target = document.getElementById( id ) ) )
+		return;
+
+	console.log( target.offsetTop )
+	ctx.scrollTo( { top: target.offsetTop, behavior: 'smooth' } )
+}
+
 (function(){
 	let eCurrentYear;
 	if ( ( eCurrentYear = document.getElementById( "CurrentYear" ) ) )
